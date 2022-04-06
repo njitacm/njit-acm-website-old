3e83dcda9e6c614cd2e6ce7494273f99923d4b74
cgr28 <cgr28@njit.edu>
2022-04-06 16:53:02 -0400

    sig components

---
+++ src/components/CurrSig.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+      <div class="card-body row">
+        <div class="col">
+          <img class="w-100 d-block" :logo="photo">
+        </div>
+        <div class="col-8" style="text-align: center;">
+          <h3>{{sigTitle}}</h3>
+          <h5>{{leader}} | {{leaderTime}}</h5>
+          <p>{{sigDesc}}</p>
+        </div>
+      </div>
+</div>
+</template>
+<script>
+  export default {
+    props:{
+        sigTitle: String,
+        sigDesc: String,
+        leader: String,
+        leaderTime: String,
+        logo: String
+    }
+  }
+</script>
---
+++ src/components/LegacySig.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="col">
+    <div class="card mb-5" style="width: 18rem;">
+        <div class="card-body">
+        <h5 class="card-title">{{sigName}}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">{{year}}</h6>
+        <h6 class="card-subtitle mb-2 text-muted">{{leader}}</h6>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+  export default {
+    props:{
+        leader: String,
+        year: String,
+        sigName: String
+    }
+  }
+</script>
